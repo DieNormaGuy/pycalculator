8533b280562b2bbac0f1697bcf710f24bedcaa2a
Samuel <jair.rios@gmail.com>
2023-11-25 12:54:40 -0700

    added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+left = int(input("enter a number "))
+right = int(input("enter a number "))
+exp = input("expression ")
+if exp == "+":
+    sum = left + right 
+    print (sum)
+elif exp == "-":
+    sum = left - right 
+    print (sum)
+elif exp == "*": 
+    sum = left * right 
+    print (sum)
+elif exp == "/":
+    sum = left / right 
+    print (sum)
+    
+else :
+    print ("invalid") 
+
+
